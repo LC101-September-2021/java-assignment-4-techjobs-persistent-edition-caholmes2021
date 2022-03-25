1999cc52d17f6a0002708490a3f3236229d43dc8
caholmes2021 <94148713+caholmes2021@users.noreply.github.com>
2022-03-24 20:40:28 -0500

    Update Employer.java

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -11,17 +11,14 @@ import java.util.List;
 @Entity
 public class Employer extends AbstractEntity {
 
-    @NotBlank(message = "Please enter a location.")
-    @Size(max = 100, message = "Please enter a shorter location name.")
+//    @ManyToOne
+    @Size(min=1, max=100)
+    @NotBlank(message = "Location is required")
     private String location;
 
-    @OneToMany
     @JoinColumn(name = "employer_id")
-    private final List<Job> jobs = new ArrayList<>();
-
-    public Employer(String location) {
-        this.location = location;
-    }
+    @OneToMany
+    private final List<Job> jobs= new ArrayList();
 
     public Employer() {
     }
@@ -33,4 +30,5 @@ public class Employer extends AbstractEntity {
     public void setLocation(String location) {
         this.location = location;
     }
+}
 }
