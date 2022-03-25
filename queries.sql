ace15ec947d62c1a53b08fc551aba0affde20316
caholmes2021 <94148713+caholmes2021@users.noreply.github.com>
2022-03-24 20:32:30 -0500

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,17 +1,30 @@
 -- Part 1: Test it with SQL
-id int PK
-employer varchar(255)
-name varchar(255)
-skills varchar(255)
+
+-- Response in Job Table
+-- --Column 1: Employer, Data Type: varchar(255)
+-- --Column 2: id, Data type: int
+-- --Column 3: name, Data Type: varchar(255)
+-- --Column 4: skills, Data Type: varchar(255)
+
+-- Sample Query to see table
+SELECT * FROM job;
+
+-- Sample Query to see columns and data types
+SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'job';
 
 -- Part 2: Test it with SQL
-SELECT name
-FROM employer
-WHERE location = "St. Louis City";
+SELECT name FROM employer WHERE location = "St. Louis City";
+
 -- Part 3: Test it with SQL
+-- Delete job table 
 DROP TABLE job;
+
 -- Part 4: Test it with SQL
-SELECT * FROM skill INNER JOIN
-    job_skills ON skill.id = job_skills.skills_id
+SELECT *
+FROM skill
+LEFT JOIN job_skills 
+ON skill.id = job_skills.skills_id
 WHERE job_skills.jobs_id IS NOT NULL
 ORDER BY name ASC;
